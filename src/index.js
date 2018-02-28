6d3bbf4cd5dbc10fc54914ff5a02be9d0630909c
Karyna <36641408+KarynaB@users.noreply.github.com>
2018-02-28 14:42:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) 
+{
   // your solution
+  var len=str.length;
+  var last = str.toString().slice(-1);
+  var last2 = str.toString().slice(-3);
+  if(len % 2 == 0 && last!="(" && last!="8" && last!="6"&& last2!="(])" && last2!="(|)" && last2!="[[]")
+    {
+    t = true;
+    return t;
+    } 
+    else
+      { 
+      t = false;
+      return t;
+      }
 }
